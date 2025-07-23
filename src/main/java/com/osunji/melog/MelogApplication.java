500c724818ba352ae7a298a077a0bc10d2a2fd0a
seajihey <okcoco03@naver.com>
2025-07-24 03:55:16 +0900

    2025/07/24 feat\#1

--- src/main/java/com/osunji/melog/MelogApplication.java
+++ src/main/java/com/osunji/melog/MelogApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MelogApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(MelogApplication.class, args);
 	}
 
